7cb0c04c685d01cef79c4e4e16e10a16f52074d9
Gino Zampa <ginojesus2002@gmail.com>
2026-05-11 20:49:11 -0300

    4to test

--- string_calculator.py
+++ string_calculator.py
@@ -2,7 +2,8 @@ def sumar(numbers: str) -> int:
     if numbers == "":
         return 0
     elif "," in numbers:
-        return 3
+        numeros = numbers.split(",")
+        return int(numeros[0]) + int(numeros[1])
     else:
         return int(numbers)
     
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -14,5 +14,8 @@ class TestStringCalculator(unittest.TestCase):
     def test_dos_numeros_devuelve_la_suma(self):
         self.assertEqual(sumar("1,2"), 3)
 
+    def test_dos_numeros_devuelve_la_suma(self):
+        self.assertEqual(sumar("5,6"), 11)
+
 if __name__ == '__main__':
     unittest.main()
