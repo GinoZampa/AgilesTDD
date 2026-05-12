efb9b46382c801545ca5f23c3a3d2d61bab5c8a0
Bruno Cascardo <brucascardo@gmail.com>
2026-05-11 21:24:28 -0300

    correccion feedback

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -8,13 +8,13 @@ class TestStringCalculator(unittest.TestCase):
     def test_un_numero_devuelve_el_mismo_numero(self):
         self.assertEqual(sumar("5"), 5)
 
-    def test_un_numero_devuelve_el_mismo_numero(self):
+    def test_un_numero_devuelve_el_mismo_numero_v2(self):
         self.assertEqual(sumar("4"), 4)
 
     def test_dos_numeros_devuelve_la_suma(self):
         self.assertEqual(sumar("1,2"), 3)
 
-    def test_dos_numeros_devuelve_la_suma(self):
+    def test_dos_numeros_devuelve_la_suma_v2(self):
         self.assertEqual(sumar("5,6"), 11)
 
     def test_tres_numeros_devuelve_la_suma(self):
